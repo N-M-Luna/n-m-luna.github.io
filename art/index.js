1b4b6fc683feb94ee8fd0bf5a1e81ba410f42c34
N. M. Luna <nat.luna.64@gmail.com>
2025-11-10 15:25:31 -0800

    write art portfolio draft

---
+++ art/index.js
@@ -0,0 +1,5 @@
+function activate(linkName) {
+    // const allLinks = document.getElementById('top-navigation')
+    // console.log(allLinks)
+  document.getElementById(linkName).classList.add('active')
+}
