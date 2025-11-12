d97a20e1ce1afc15bbb4306cc038fc2b9d33feaf
N. M. Luna <nat.luna.64@gmail.com>
2025-11-11 16:52:03 -0800

    add some style to art page

--- art/index.js
+++ art/index.js
@@ -1,5 +1,12 @@
+//For top navigational bar
 function activate(linkName) {
     // const allLinks = document.getElementById('top-navigation')
     // console.log(allLinks)
-  document.getElementById(linkName).classList.add('active')
+  //document.getElementById(linkName).classList.add('active')
+}
+
+// Back to top button
+function takeMeToTheTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
